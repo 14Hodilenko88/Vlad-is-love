5930640ab4cfa39b0b5653211ad3c88d84be20e7
14Hodilenko88 <vladrumil88@gmail.com>
2024-10-23 15:03:35 +0300

    777

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from datetime import datetime
 app = Flask(__name__)
 db = SQLAlchemy(app)  # Ініціалізація SQLAlchemy
 
+
+
 class Student(db.Model):
     student_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
